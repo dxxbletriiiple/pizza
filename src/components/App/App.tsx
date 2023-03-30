e5ed538db84686ebd5e4473c8c4d60ccf8f62de0
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-03-30 23:20:55 +0300

    edit: props on piiza item

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -16,11 +16,11 @@ export const App = () => {
 					</div>
 					<h2 className='content__title'>Все пиццы</h2>
 					<div className='content__items'>
-						<PizzaItem />
-						<PizzaItem />
-						<PizzaItem />
-						<PizzaItem />
-						<PizzaItem />
+						<PizzaItem title='qwqew' price={500} />
+						<PizzaItem title='qwqew' price={500} />
+						<PizzaItem title='qwqew' price={500} />
+						<PizzaItem title='qwqew' price={500} />
+						<PizzaItem title='qwqew' price={500} />
 					</div>
 				</div>
 			</div>
