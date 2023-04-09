9d5a0ba47f18db12e82709ada7a80aae9d378eb5
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-04-10 02:26:47 +0300

    edit: spinner

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Categories from '../Categories';
 import Header from '../Header';
 import PizzaItem from '../PizzaItem';
 import Sort from '../Sort';
+import { GetPizza } from '../../services/GetPizza.service';
+import { IPizza } from '../../interfaces';
 import './App.scss';
+import Spinner from '../Spinner';
 
-import { data } from '../../utils/data';
+const gp = new GetPizza();
 
 export const App = () => {
-	const [pizzaArray, setPizzaArray] = useState(data);
+	const [pizzaArray, setPizzaArray] = useState([]);
 	const [pizzaCount, setPizzaCount] = useState(0);
 
+	useEffect(() => {
+		gp.getPizza().then(setPizzaArray);
+	}, []);
+
 	const handleClick = () => {
-		console.log('qqq');
 		setPizzaCount(pizzaCount + 1);
 	};
 	return (
@@ -25,11 +31,15 @@ export const App = () => {
 						<Sort />
 					</div>
 					<h2 className='content__title'>Все пиццы</h2>
-					<div className='content__items'>
-						{pizzaArray.map((el) => (
-							<PizzaItem {...el} handleClick={handleClick} key={el.id} />
-						))}
-					</div>
+					{!pizzaArray[0] ? (
+						<Spinner />
+					) : (
+						<div className='content__items'>
+							{pizzaArray.map((el: IPizza) => (
+								<PizzaItem {...el} handleClick={handleClick} key={el.id} />
+							))}
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
