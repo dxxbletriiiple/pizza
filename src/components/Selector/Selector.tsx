c6e6e8c0ca07232e127a1fb9f02569670e2c01cc
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-04-09 22:33:45 +0300

    add: selector

---
+++ src/components/Selector/Selector.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { ISelector } from './Selector.interface';
+
+export const Selector = ({ arr }: ISelector) => {
+	const [selectedIndex, setSelectedIndex] = useState(0);
+
+	const handleClick = (i: number) => {
+		setSelectedIndex(i);
+	};
+
+	return (
+		<ul>
+			{arr.map((el: number | string, i: number) => (
+				<li className={selectedIndex === i ? 'active' : ''} onClick={() => handleClick(i)} key={i}>
+					{el}
+				</li>
+			))}
+		</ul>
+	);
+};
