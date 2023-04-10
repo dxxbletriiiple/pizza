1f061b5ac4504b9fcc1744439f7b9e9de81aa29e
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-04-10 06:43:16 +0300

    edit: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,7 +15,7 @@ export const Header = ({ pizzaCount }: IHeader): JSX.Element => {
 				</div>
 				<div className='header__cart'>
 					<a href='#!' className='button button--cart'>
-						<span>520 ₽</span>
+						<span>0 ₽</span>
 						<div className='button__delimiter'></div>
 						<svg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
 							<path
