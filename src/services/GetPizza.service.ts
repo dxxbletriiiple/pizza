9b560c42eeeafc1ce220e267b15c6daf7a13a319
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-04-13 12:23:10 +0300

    edit: pizza services add categories var

--- src/services/GetPizza.service.ts
+++ src/services/GetPizza.service.ts
@@ -1,7 +1,9 @@
 export class GetPizza {
-	BASE_URL = 'https://64333a681c5ed06c957c8aee.mockapi.io/api/v1/items';
+	private BASE_URL = 'https://64333a681c5ed06c957c8aee.mockapi.io/api/v1/';
+	private ALL_PIZZAS = 'items';
+	private CATEGORIES = 'categories';
 
 	public fetchData = (rest: string = '') => {
-		return fetch(this.BASE_URL + rest).then((r) => r.json());
+		return fetch(this.BASE_URL + this.ALL_PIZZAS + rest).then((r) => r.json());
 	};
 }
