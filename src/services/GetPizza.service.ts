f3324bf369c6405777d6bfc094299ade563b05ce
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-04-13 12:03:37 +0300

    refactor: pizza service

--- src/services/GetPizza.service.ts
+++ src/services/GetPizza.service.ts
@@ -1,5 +1,7 @@
 export class GetPizza {
-	public getPizza = () => {
-		return fetch('https://64333a681c5ed06c957c8aee.mockapi.io/api/v1/items').then((r) => r.json());
+	BASE_URL = 'https://64333a681c5ed06c957c8aee.mockapi.io/api/v1/items';
+
+	public fetchData = (rest: string = '') => {
+		return fetch(this.BASE_URL + rest).then((r) => r.json());
 	};
 }
