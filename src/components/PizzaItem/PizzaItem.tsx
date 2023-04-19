f35c4ae4027c2f5cb5a02409964f6f6796fe3c3d
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-04-19 19:34:13 +0300

    refactor: pizza item

--- src/components/PizzaItem/PizzaItem.tsx
+++ src/components/PizzaItem/PizzaItem.tsx
@@ -1,25 +1,23 @@
 import { IPizzaItem } from './PizzaItem.interface';
 import Button from '../Button';
-import './PizzaItem.scss';
 import Selector from '../Selector';
+import styles from './PizzaItem.module.scss';
 
-export const PizzaItem = ({ imageUrl, title, types, sizes, price, category, rating, handleClick }: IPizzaItem): JSX.Element => {
-	const typeNames: string[] = ['тонкое', 'традиционное'];
-
+export const PizzaItem = ({ id, imageUrl, title, types, sizes, price, category, rating, handleClick }: IPizzaItem): JSX.Element => {
 	return (
-		<div className='pizza-block'>
-			<img className='pizza-block__image' src={imageUrl} alt={title} />
-			<h4 className='pizza-block__title'>{title}</h4>
-			<div className='pizza-block__selector'>
+		<div className={styles.pizza}>
+			<img className={styles.image} src={imageUrl} alt={title} />
+			<h4 className={styles.title}>{title}</h4>
+			<div className={styles.selector}>
 				<ul>
-					<Selector arr={typeNames} />
+					<Selector id={id} arr={types} />
 				</ul>
 				<ul>
-					<Selector arr={sizes} />
+					<Selector id={id} arr={sizes} />
 				</ul>
 			</div>
-			<div className='pizza-block__bottom'>
-				<div className='pizza-block__price'>от {price} ₽</div>
+			<div className={styles.bottom}>
+				<div className={styles.price}>от {price} ₽</div>
 				<Button handleClick={handleClick} />
 			</div>
 		</div>
