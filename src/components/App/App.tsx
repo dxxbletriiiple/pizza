28e65dea9ff2608443d5b3fc6ef618552879cba0
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-04-19 22:47:05 +0300

    refactor: app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import { Home, NotFound } from '../../pages';
 import Cart from '../Cart';
 import Header from '../Header';
-import './App.scss';
+import st from './App.module.scss';
 
 export const App = () => {
 	return (
-		<div className='wrapper'>
+		<div className={st.wrapper}>
 			<Header pizzaCount={0} />
-			<div className='content'>
-				<div className='container'>
+			<div className={st.content}>
+				<div className={st.container}>
 					<Routes>
 						<Route path='/' element={<Home />} />
 						<Route path='/cart' element={<Cart />} />
